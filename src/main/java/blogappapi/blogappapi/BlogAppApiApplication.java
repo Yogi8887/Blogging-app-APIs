15794b6b8a979367dc17808fea1ae74c705ea081
Yogi8887 <yogi.nit.mca@gmail.com>
2022-07-02 18:52:36 +0530

    blog app

---
+++ src/main/java/blogappapi/blogappapi/BlogAppApiApplication.java
@@ -0,0 +1,14 @@
+package blogappapi.blogappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApiApplication.class, args);
+		System.out.println("Hllo");
+	}
+
+}
