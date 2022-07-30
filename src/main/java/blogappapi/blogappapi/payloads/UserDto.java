bb8e78413e406ddd3480d83d39ba1d1f89c97f85
Yogi8887 <yogi.nit.mca@gmail.com>
2022-07-31 01:30:48 +0530

    JWT Role Authentication api complete.

--- src/main/java/blogappapi/blogappapi/payloads/UserDto.java
+++ src/main/java/blogappapi/blogappapi/payloads/UserDto.java
@@ -1,6 +1,7 @@
 package blogappapi.blogappapi.payloads;
 
 
+import blogappapi.blogappapi.entities.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -26,4 +29,6 @@ public class UserDto {
     private String password;
     @NotNull
     private String about;
+
+    private Set<RoleDto> roles = new HashSet<>();
 }
